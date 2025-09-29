53d62cc2204aed55b4aa299946f9d0fd425de41d
epicuser <clement.t@hom-e.fr>
2025-09-29 22:50:19 +0200

    chore: deps APT via helpers

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,4 +2,4 @@
 # Common variables and dependencies for AFFiNE YunoHost package
 
 # APT dependencies required for AFFiNE build and runtime
-pkg_dependencies="redis-server postgresql build-essential python3 make g++"
+pkg_dependencies="build-essential python3 make g++"
